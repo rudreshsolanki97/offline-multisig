f0907a9244b73d494e49992a2b763bf6a1e109eb
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2021-06-09 18:23:44 +0530

    initial commit for UI boilerplate

---
+++ frontend/ui/src/App.js
@@ -0,0 +1,20 @@
+  import './App.css';
+
+import {AccountFetcher} from "./components/AccountReader";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <div>
+          <AccountFetcher />
+        </div>
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
